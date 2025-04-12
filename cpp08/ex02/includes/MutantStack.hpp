59e67391dd6e9938c8633f40fd08d39ab6247554
Nicolas Premont <ni.premont@gmail.com>
2025-04-12 17:45:17 +0200

    cpp08ex02

--- cpp08/ex02/includes/MutantStack.hpp
+++ cpp08/ex02/includes/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: npremont <npremont@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/12 12:42:50 by npremont          #+#    #+#             */
-/*   Updated: 2025/04/12 12:46:44 by npremont         ###   ########.fr       */
+/*   Updated: 2025/04/12 17:41:48 by npremont         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,51 @@
 
 # include <stack>
 # include <iostream>
+# include <list>
 
-class MutantStack {
-    private:
-        std::stack<int> waf;
-
+template <typename T>
+class MutantStack : public std::stack<T> {
     public:
+        MutantStack()
+        {}
 
+        MutantStack(const MutantStack& other)
+        {
+            *this = other;
+        }
+
+        MutantStack& operator=(const MutantStack& other)
+        {
+            if (this != &other)
+                std::stack<T>::operator=(other);
+            return (*this);
+        }
+
+        ~MutantStack()
+        {}
+
+        typedef typename std::deque<T>::iterator        iterator;
+        typedef typename std::deque<T>::const_iterator  const_iterator;
+
+        iterator begin()
+        {
+            return (this->c.begin());
+        }
+
+        iterator end()
+        {
+            return (this->c.end());
+        }
+
+        const_iterator begin() const
+        {
+            return (this->c.begin());
+        }
+
+        const_iterator end() const
+        {
+            return (this->c.end());
+        }
 };
 
 #endif
--- cpp08/ex02/srcs/main.cpp
+++ cpp08/ex02/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: npremont <npremont@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/12 12:46:52 by npremont          #+#    #+#             */
-/*   Updated: 2025/04/12 12:49:32 by npremont         ###   ########.fr       */
+/*   Updated: 2025/04/12 17:44:35 by npremont         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,65 @@
 
 int main(void)
 {
-    std::stack<int> stack;
+    MutantStack<int> stack;
 
-    stack.push(11);
-    stack.push(7);
     stack.push(12);
+    stack.push(2);
+    stack.push(15);
 
-    std::cout << stack.top() << std::endl;
+    std::deque<int>::iterator iter;
+
+    for (iter = stack.begin(); iter < stack.end(); iter++)
+    {
+        std::cout << *iter << std::endl;
+    }
+
+    {
+        std::cout <<std::endl << "official test" << std::endl;
+
+        MutantStack<int> mstack;
+        mstack.push(5);
+        mstack.push(17);
+        std::cout << mstack.top() << std::endl;
+        mstack.pop();
+        std::cout << mstack.size() << std::endl;
+        mstack.push(3);
+        mstack.push(5);
+        mstack.push(737);
+        mstack.push(0);
+        MutantStack<int>::iterator it = mstack.begin();
+        MutantStack<int>::iterator ite = mstack.end();
+        ++it;
+        --it;
+        while (it != ite)
+        {
+            std::cout << *it << std::endl;
+            ++it;   
+        }
+        std::stack<int> s(mstack);
+    }
+
+    {
+        std::cout << std::endl << "official test 2" << std::endl;
+
+        std::list<int> mstack;
+        mstack.push_back(5);
+        mstack.push_back(17);
+        std::cout << mstack.back() << std::endl;
+        mstack.pop_back();
+        std::cout << mstack.size() << std::endl;
+        mstack.push_back(3);
+        mstack.push_back(5);
+        mstack.push_back(737);
+        mstack.push_back(0);
+        std::list<int>::iterator it = mstack.begin();
+        std::list<int>::iterator ite = mstack.end();
+        ++it;
+        --it;
+        while (it != ite)
+        {
+            std::cout << *it << std::endl;
+            ++it;   
+        }
+    }
 }
