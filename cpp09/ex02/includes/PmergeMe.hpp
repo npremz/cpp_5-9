7b1f22e1c600d4c0935de65189326f0f25139495
nico <ni.premont@gmail.com>
2025-04-29 21:16:46 +0200

    LIBRE

--- cpp09/ex02/includes/PmergeMe.hpp
+++ cpp09/ex02/includes/PmergeMe.hpp
@@ -17,17 +17,25 @@
 # include <string>
 # include <sstream>
 # include <vector>
+# include <deque>
 # include <algorithm>
 # include <cmath>
+# include <sys/time.h>
+# include <iomanip>
 
-# define DEBUG 1 
+# define DEBUG 0
 
 template <typename C>
 class PmergeMe {
     private:
-        C container;
+        C               container;
          
     public:
+        PmergeMe()
+        {
+            throw std::runtime_error("no argument");
+        }
+
         PmergeMe(char **av)
         {
             std::string sequence_str;
@@ -42,29 +50,26 @@ class PmergeMe {
             while (sequence_stream >> n)
             {
                 if (std::find(container.begin(), container.end(), n) != container.end())
-                    throw std::runtime_error("duplicated values\n");
+                    throw std::runtime_error("duplicated values as argument");
                 container.push_back(n);
             }
         }
        
         ~PmergeMe()
         {}
+
+        C   getContainer()
+        {
+            return (this->container);
+        }
         
-        void printC(int order = 1)
+        void printC()
         {
             typename C::iterator start;
-            int i = 0;
-
-            std::cout << "order " << order << ": ";
             for (start = container.begin(); start != container.end(); start++)
             {
-                if (i % order == 0)
-                    std::cout << "[";
                 std::cout << *start;
-                if ((i + 1) % order == 0)
-                    std::cout << "]";
                 std::cout << " ";
-                ++i;
             }
             std::cout << std::endl;
         }
@@ -76,13 +81,6 @@ class PmergeMe {
 
         void insert(C& main, C& pend, C&rest, int order)
         {
-            // si pend vide, on sort
-
-            (void)main;
-            (void)pend;
-            (void)rest;
-            (void)order;
-
             if (pend.empty())
                 return;
 
@@ -99,56 +97,30 @@ class PmergeMe {
                 pend_upper.push_back(*it);
             }
 
-            std::cout << "\t main_u: ";
-            for (typename C::iterator it = main_upper.begin(); it < main_upper.end(); it++)
-            {
-                std::cout << *it << ", ";
-            }
-
-            std::cout << "\t pend_u: ";
-            for (typename C::iterator it = pend_upper.begin(); it < pend_upper.end(); it++)
-            {
-                std::cout << *it << ", ";
-            }
-
-            // Definir la valeur jacobsthal
             int k = 2;
             while (!pend_upper.empty())
             {
                 int jc = jacobsthal(k);
                 unsigned int idx = jc - 1;
-                
-                std::cout << "\n\tk: " << k << ", jc: " << jc << std::endl;
-
-                std::cout << "\t main: ";
-
-                for (typename C::iterator it = main.begin(); it < main.end(); it++)
-                {
-                    std::cout << *it << ", ";
-                }
-
-                std::cout << "\n\t pend: ";
-
-                for (typename C::iterator it = pend.begin(); it < pend.end(); it++)
-                {
-                    std::cout << *it << ", ";
-                }
-                std::cout << std::endl;
-
                 if (idx > pend_upper.size())
                     idx = pend_upper.size();
-                typename C::iterator end = main_upper.end();
                 while (idx)
                 {
-                    if (main_upper.size() < idx * 2 - 1)
-                        end = main_upper.begin() + idx * 2 - 1;
-                    std::cout << "value to upper:" << *(pend_upper.begin() + idx - 1) << std::endl;
-                    std::cout << "value found: " << *std::upper_bound(main_upper.begin(), end, *(pend_upper.begin() + idx - 1)) << std::endl;
+                    typename C::iterator ip = std::upper_bound(main_upper.begin(), main_upper.end(), *(pend_upper.begin() + idx - 1));
+                    int ref = *ip;
 
-                    typename C::iterator ip = std::upper_bound(main_upper.begin(), end, *(pend_upper.begin() + idx - 1));
-                    std::cout << "ip: " << *ip << std::endl;
                     main_upper.insert(ip, *(pend_upper.begin() + (idx - 1)));
                     pend_upper.erase(pend_upper.begin() + idx - 1);
+
+                    typename C::iterator it = std::find(main_upper.begin(), main_upper.end(), ref);
+                    int position = std::distance(main_upper.begin(), it);
+                    int count = 0;
+                    for (typename C::iterator it = pend.begin() + (idx - 1) * order; it < pend.begin() + (idx * order); it++)
+                    {
+                        main.insert(main.begin() + ((position - 1) * order) + count, *it);
+                        count++;
+                    }
+                    pend.erase(pend.begin() + (idx - 1) * order, pend.begin() + (idx * order));
                     idx--;
                 }
                 k++;
@@ -158,15 +130,6 @@ class PmergeMe {
             rest.clear();
 
             this->container = main;
-
-            // Inserer en reverse order les [b] a partir du [b] jc
-            // comparer jusqu'au [a] correspondant si pas present, comparer avec tout
-            // comparer l'element le plus grand de la paire (le dernier) avec ceux des autres paires
-
-            // ajouter le reste a la fin de la boucle
-
-
-
         }
 
         void sort(int order = 1)
@@ -174,9 +137,6 @@ class PmergeMe {
             size_t unit_count = this->container.size() / order;
             if (unit_count < 2)
                 return;
-            
-            if (DEBUG)
-                this->printC(order);
 
             int                     is_count_odd = unit_count % 2 == 1;
             typename C::iterator    start = this->container.begin();
@@ -191,12 +151,6 @@ class PmergeMe {
                         std::swap(*(it + i), *(it + i + order));
                     }
                 }
-            }
-
-            if (DEBUG)
-            {
-                this->printC(order);
-                std::cout << std::endl;
             }
 
             sort(order * 2);
@@ -230,54 +184,8 @@ class PmergeMe {
 
             rest.insert(rest.begin(), end + (order * is_count_odd), this->container.end());
 
-            if (DEBUG)
-            {
-                std::cout << "order " << order << ": ";
-                std::cout << "main: [";
-                for (typename C::iterator main_start = main.begin(); main_start < main.end(); main_start++)
-                {
-                    std::cout << *main_start << " ";
-                }
-                std::cout << "] ";
-                std::cout << "pend: [";
-                for (typename C::iterator pend_start = pend.begin(); pend_start < pend.end(); pend_start++)
-                {
-                    std::cout << *pend_start << " ";
-                }
-                std::cout << "] ";
-                std::cout << "rest: [";
-                for (typename C::iterator rest_start = rest.begin(); rest_start < rest.end(); rest_start++)
-                {
-                    std::cout << *rest_start << " ";
-                }
-                std::cout << "]" << std::endl;
-            }
-
             if (!pend.empty())
                 insert(main, pend, rest, order);
-
-            if (DEBUG)
-            {
-                std::cout << "order " << order << ": ";
-                std::cout << "main: [";
-                for (typename C::iterator main_start = main.begin(); main_start < main.end(); main_start++)
-                {
-                    std::cout << *main_start << " ";
-                }
-                std::cout << "] ";
-                std::cout << "pend: [";
-                for (typename C::iterator pend_start = pend.begin(); pend_start < pend.end(); pend_start++)
-                {
-                    std::cout << *pend_start << " ";
-                }
-                std::cout << "] ";
-                std::cout << "rest: [";
-                for (typename C::iterator rest_start = rest.begin(); rest_start < rest.end(); rest_start++)
-                {
-                    std::cout << *rest_start << " ";
-                }
-                std::cout << "]" << std::endl << std::endl;
-            }
         }
 };
 
--- cpp09/ex02/srcs/main.cpp
+++ cpp09/ex02/srcs/main.cpp
@@ -12,17 +12,64 @@
 
 #include "../includes/PmergeMe.hpp"
 
+void    print_duration(struct timeval debut, struct timeval fin, int size, std::string type)
+{
+    long seconds = fin.tv_sec - debut.tv_sec;
+    long microseconds = fin.tv_usec - debut.tv_usec;
+    
+    if (microseconds < 0) {
+        microseconds += 1000000;
+        seconds--;
+    }
+    
+    double total_microseconds = seconds * 1000000 + microseconds;
+    
+    std::cout << std::fixed << std::setprecision(5);
+    std::cout << "Time to process a range of " 
+              << size 
+              << " elements with std::" 
+              << type << " " 
+              << total_microseconds / 1000000 << " us" << std::endl;
+}
+
 int main(int ac, char* av[])
 {
-    (void)ac;
-    PmergeMe<std::vector<int> > test(av);
+    if (ac < 2)
+    {
+        std::cerr << "This program needs integers as arguments to run." << std::endl;
+        return (1);
+    }
+
     try 
     {
-        test.sort();
+        PmergeMe<std::vector<int> > vectorTest(av);
+        PmergeMe<std::deque<int> > dequeTest(av);
+
+        struct timeval debut_vector, fin_vector;
+        struct timeval debut_deque, fin_deque;
+
+        std::cout << "Before: ";
+        vectorTest.printC();
+
+        gettimeofday(&debut_vector, NULL);
+        vectorTest.sort();
+        gettimeofday(&fin_vector, NULL);
+        
+        gettimeofday(&debut_deque, NULL);
+        dequeTest.sort();
+        gettimeofday(&fin_deque, NULL);
+
+        std::cout << "After: ";
+        vectorTest.printC();
+
+        print_duration(debut_vector, fin_vector , vectorTest.getContainer().size(), "vector");
+        print_duration(debut_deque, fin_deque , dequeTest.getContainer().size(), "deque");
     }
     catch( const std::exception& e)
     {
-        std::cerr << e.what() << std::endl;
+        std::cerr << "Error: " << e.what() << std::endl;
     }
+
+    return (0);
 
 }
